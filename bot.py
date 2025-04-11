051e5b1e09cdeb1f6224e679da29b308442147df
lipeaaraujo <felipearaujodff@gmail.com>
2025-04-11 12:48:52 -0300

    adding better typing and remaking deletar_roles command

--- bot.py
+++ bot.py
@@ -1,43 +1,55 @@
 import os
 from typing import Final
-import discord
+from discord import Client, Intents, Message, Role
 from dotenv import load_dotenv
 from discord.ext import commands
 
 load_dotenv()
 TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
 
-intents = discord.Intents.default()
+intents: Intents = Intents.default()
 intents.guilds = True
 intents.guild_messages = True
 intents.message_content = True
 intents.guilds = True
 intents.members = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
-
-# Lista dos nomes dos roles que você quer deletar
-roles_para_deletar = ["new role"]
+# init bot
+bot: commands.Bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"Bot conectado como {bot.user}")
+    print(f"Bot conectado como {bot.user}!")
 
 @bot.command()
 @commands.has_permissions(manage_roles=True)
-async def deletar_roles(ctx):
+async def deletar_roles(ctx: commands.Context, *, role_name: str):
+    """Deleta um cargo do servidor com base no nome fornecido."""
     guild = ctx.guild
-    canal = discord.utils.get(guild.text_channels, name="bot")
-    if canal:
-        for role in guild.roles:
-            if role.name in roles_para_deletar:
-                try:
-                    await role.delete()
-                    await canal.send(f"Role deletada: {role.name}")
-                except Exception as e:
-                    await canal.send(f"Erro ao deletar {role.name}: {e}")
+    channel = ctx.channel
+    # channel = discord.utils.get(guild.text_channels, name="bot")
+    
+    if not channel:
+        print("Canal não encontrado")
+        return
+
+    if channel.name != "bot":
+        print("Bot só pode ser chamado no canal bot")
+        return
+
+    count = 0
+    for role in guild.roles:
+        if role.name == role_name:
+            try:
+                await role.delete()
+                print(f"Role deletada: {role.name}")
+                count += 1
+            except Exception as e:
+                await channel.send(f"Erro ao deletar {role.name}: {e}")
+    
+    if count > 0:
+        await channel.send(f"{count} roles foram deletadas com sucesso!")
     else:
-        await ctx.send("Canal não encontrado.")
-        
+        await channel.send(f'Nenhuma role foi encontrada com o nome "{role_name}"')
 
 bot.run(TOKEN)
